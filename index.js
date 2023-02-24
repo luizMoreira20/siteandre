e011ee067bc0f3ff8cad16bef772d45d44f84eec
luizMoreira20 <126097310+luizMoreira20@users.noreply.github.com>
2023-02-24 12:07:28 -0300

    commit inicial
    primeiro commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const ourItemDiv = document.getElementsByClassName("item");
+const openIcon = document.getElementsByClassName("iconOpen");
+const closeIcon = document.getElementsByClassName("iconClose");
+
+// console.log(ourItemDiv);
+
+for (let i = 0; i < ourItemDiv.length; i++) {
+  closeIcon[i].style.display = "none";
+
+  ourItemDiv[i].addEventListener("click", () => {
+    const result = ourItemDiv[i].classList.toggle("active");
+
+    if (result) {
+      closeIcon[i].style.display = "block";
+      openIcon[i].style.display = "none";
+    } else {
+      closeIcon[i].style.display = "none";
+      openIcon[i].style.display = "block";
+    }
+  });
+}
+
+
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
